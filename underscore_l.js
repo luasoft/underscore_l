aa6c1cac49d488a9db3d5278be91f0d098435085
luasoft <doanluan0909@gmail.com>
2015-06-16 17:15:26 +0700

    1.0.Alpha1

---
+++ underscore_l.js
@@ -0,0 +1,57 @@
+var _l = function(obj) {
+  this.obj = obj;
+}
+
+// Undercore L library version
+_l.VERSION = '1.0.Alpha1';
+
+// return an element by index of an array
+_l.getElementByIndex = function(arr, index) {
+  var length = arr.length;
+  if (0 == length) return null;
+  var el = null, i = 0;
+  for (; i < length; i++) {
+    if (index === i) {
+      el = arr[i];
+      break;
+    }
+  }
+  return el;
+};
+
+// return the maximum value of an array
+_l.max = function(arr) {
+  var length = arr.length;
+  if (0 == length) return null;
+  var max = arr[0], i = 1;
+  for (; i < length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return max;
+};
+
+// return the minimum value of an array
+_l.min = function(arr) {
+  var length = arr.length;
+  if (0 == length) return null;
+  var min = arr[0], i = 1;
+  for (; i < length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+  return min;
+};
+
+// check an object is defined or not
+_l.isUndefined = function(obj) {
+  return ('undefined' === typeof obj);
+};
+
+// check an object is an object or not
+_l.isObject = function(obj) {
+  return ('object' === typeof obj);
+};
+  
